7ff3ae969776fe0138019f18cc181f0260359760
ravindra murge <ravindramurge319@gmail.com>
2023-12-05 18:48:02 +0530

    first java program committed

---
+++ src/sampleProject/HelloWorld.java
@@ -0,0 +1,11 @@
+package sampleProject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to the world of java");
+	}
+
+}
